9a08427978727f6a729ced778cba3b5a5a67744d
Kevin Landry <kevinjlandry@gmail.com>
2020-07-19 16:38:42 -0500

    Initial Commit

---
+++ GitTest2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTest2
+//
+//  Created by Kevin Landry on 7/19/20.
+//  Copyright © 2020 Kevin Landry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
